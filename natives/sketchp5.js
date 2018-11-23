df1ba42948118c270113e14556b7718e5cdbb61d
Pedro Juan <petertitoy@gmail.com>
2018-11-24 01:13:12 +0800

    added vue2-leaflet and config pages/index.vue

--- natives/sketchp5.js
+++
@@ -1,9 +0,0 @@
-export default function (sketch) {
-  sketch.setup(){
-
-  }
-  sketch.draw(){
-
-  }
-
-}
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
   <v-content>
     <h1>Hello world!</h1>
-    <v-layout> 
+    <v-layout>
+      <v-flex
+        id="mapid"
+        xs12
+        sm8
+        md7
+        lg5/>
+      <v-flex
+        xs12
+        sm3
+        md4
+        lg4
+        offset-lg2
+        offset-xs0>
+        
+      </v-flex>
     </v-layout>
   </v-content>
 </template>
 <script>
 </script>
+<style scoped>
+#mapid {
+  height: 200px;
+}
+</style>
